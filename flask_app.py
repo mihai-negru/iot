51537d8b67a6ed5c7ad28c81f4242c71d5dc2a2f
mihai-negru <determinant289@gmail.com>
2024-12-10 21:48:34 +0200

    Simple

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,32 @@
 #!/bin/usr/python
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from datetime import datetime
+import threading
+import json
 
 app = Flask(__name__)
+
+DATA_FILE = 'data/pico.json'
+data_lock = threading.Lock()
+
+def read_data():
+    try:
+        with open(DATA_FILE, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+    
+def write_data(data):
+    with open(DATA_FILE, "w") as file:
+        return json.dump(data, file)
+
+@app.route('/sensor_data', methods = ['GET'])
+def sensor_data():
+    with data_lock:
+        data = read_data()
+
+    return jsonify(data)
 
 @app.route('/')
 def homepage():
@@ -11,14 +34,24 @@ def homepage():
 
 @app.route('/gas_sensor', methods=['POST'])
 def post_value():
-    value = request.json.get("value")
-    if value is not None:
-        
-        
-        return {"status": "success"}, 200
-    return {"error": "Invalid value"}, 400
+    content = request.json
+    if 'value' in content:
+        data_entry = {
+            "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "value": content['value']
+        }
 
-# @app.route('/sensor_data', methods=['GET'])
+        with data_lock:
+            data = read_data()
+            
+        data.append(data_entry)
+        if len(data) > 1000:
+            data = data[-1000:]
+        
+        with data_lock:
+            write_data(data)
+        return jsonify({"status": "success"}), 200
+    return jsonify({"error": "Invalid payload"}), 400
 
 if __name__ == "__main__":
     app.run()
