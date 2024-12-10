ae38c6027528a8f48acbc2273fc2b0e532f79a74
mihai-negru <determinant289@gmail.com>
2024-12-10 23:02:34 +0200

    replace from value to values

--- flask_app.py
+++ flask_app.py
@@ -36,23 +36,27 @@ def homepage():
 @app.route('/gas_sensor', methods=['POST'])
 def post_value():
     content = request.json
-    if 'value' in content:
-        data_entry = {
-            'x': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            'y': content['value']
-        }
 
-        with data_lock:
-            data = read_data()
-            
-        data.append(data_entry)
-        if len(data) > 1000:
-            data = data[-1000:]
-        
-        with data_lock:
-            write_data(data)
-        return jsonify({"status": "success"}), 200
-    return jsonify({"error": "Invalid payload"}), 400
+    if 'values' not in content:
+        jsonify({"error": "Invalid payload"}), 400
+
+    with data_lock:
+        data = read_data()
+
+    for item in content['values']:
+        data.append({
+            'x': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+            'y': item
+        })
+
+    if len(data) > 1000:
+        data = data[-1000:]
+
+    with data_lock:
+        write_data(data)
+
+    return jsonify({"status": "success"}), 200
+
 
 if __name__ == "__main__":
     os.remove(DATA_FILE)
