5c10c51d85f1e870e9f11315f66e380dc6bf8b7e
mihai-negru <determinant289@gmail.com>
2024-12-11 21:48:39 +0200

    change sensor limit

--- flask_app.py
+++ flask_app.py
@@ -5,14 +5,15 @@ from flask_caching import Cache
 import threading
 import json
 from datetime import datetime, timedelta
-import os
 import uuid
-import shutil
+
+DEFAULT_SENSOR_LIMIT = 45000
 
 DATA_FILE = '/var/www/html/iot/data'
 TOKEN_KEY = 'TOKENS'
 TOKEN_SENSOR_KEY = 'TOKEN_SENSOR'
 TOKEN_DATA_KEY = 'TOKEN_DATA'
+TOKEN_LIMIT_KEY = 'TOKEN_LIMIT'
 
 config = {
     'CACHE_TYPE': 'FileSystemCache',
@@ -35,6 +36,33 @@ def __cache_set(key, value, timeout=0):
     value = json.dumps(value)
     cache.set(key, value, timeout = timeout)
 
+def __validate_token(token):
+    tokens = __cache_get(TOKEN_KEY)
+    if tokens is None:
+        return False
+    
+    return token in tokens
+
+def __validate_sensor(token, sensor):
+    sensor_ip = __cache_get(f'{TOKEN_SENSOR_KEY}_{token}')
+    if sensor_ip is None:
+        return False
+
+    return sensor is None or sensor == sensor_ip
+
+def __read__token_data(token):
+    content = __cache_get(f'{TOKEN_DATA_KEY}_{token}')
+    if content is None:
+        return []
+    
+    return content
+    
+def __write_token_data(token, data):
+    __cache_set(f'{TOKEN_DATA_KEY}_{token}', data)
+
+def __update_token_sensor_time(token, sensor_ip):
+    __cache_set(f'{TOKEN_SENSOR_KEY}_{token}', sensor_ip, timeout=600)
+
 @app.route('/', methods = ['GET'])
 def homepage():
     return render_template(template_name_or_list = 'index.html')
@@ -50,25 +78,12 @@ def generate_token():
         tokens.append(new_token)
 
     __cache_set(TOKEN_KEY, tokens)
+    __cache_set(f'{TOKEN_LIMIT_KEY}_{new_token}', DEFAULT_SENSOR_LIMIT)
 
     return render_template(
         template_name_or_list = 'generated_token.html',
         token = new_token
     )
-
-def __validate_token(token):
-    tokens = __cache_get(TOKEN_KEY)
-    if tokens is None:
-        return False
-    
-    return token in tokens
-
-def __validate_sensor(token, sensor):
-    sensor_ip = __cache_get(f'{TOKEN_SENSOR_KEY}_{token}')
-    if sensor_ip is None:
-        return False
-
-    return sensor is None or sensor == sensor_ip
 
 @app.route('/view', methods = ['GET'])
 def view():
@@ -88,19 +103,8 @@ def view():
 
     return render_template(
         template_name_or_list = 'view.html',
-        token = token,
-        limit = 45000
+        token = token
     )
-
-def __read__token_data(token):
-    content = __cache_get(f'{TOKEN_DATA_KEY}_{token}')
-    if content is None:
-        return []
-    
-    return content
-    
-def __write_token_data(token, data):
-    __cache_set(f'{TOKEN_DATA_KEY}_{token}', data)
 
 @app.route('/view_data/<token>', methods = ['GET'])
 def view_data(token):
@@ -112,9 +116,6 @@ def view_data(token):
     
     data = __read__token_data(token)
     return jsonify(data), 200
-
-def __update_token_sensor_time(token, sensor_ip):
-    __cache_set(f'{TOKEN_SENSOR_KEY}_{token}', sensor_ip, timeout=600)
 
 @app.route('/update_data/<token>', methods = ['POST'])
 def update_data(token):
@@ -143,7 +144,7 @@ def update_data(token):
 
     return jsonify({'success': 'Data successfully updated'}), 200
 
-@app.route('/alive/<token>', methods=['POST'])
+@app.route('/alive/<token>', methods = ['POST'])
 def alive(token):
     if not __validate_token(token):
         return jsonify({'error': f'Bad token {token}'}), 405
@@ -151,6 +152,38 @@ def alive(token):
     __update_token_sensor_time(token, request.remote_addr)
 
     return jsonify({'success': f'Registered sensor for token {token} for 10 min'}), 200
+
+@app.route('/sensor_limit/<token>', methods = ['GET'])
+def sensor_limit(token):
+    if not __validate_token(token):
+        return render_template(
+            template_name_or_list = 'error.html',
+            message = f'Token {token} is not registered'
+        )
+    
+    if not __validate_sensor(token, None):
+        return render_template(
+            template_name_or_list = 'error.html',
+            message = f'Token {token} has no sensor attached'
+        )
+    
+    return render_template(
+        template_name_or_list = 'change_limit.html',
+        token = token
+    )
+
+@app.route('/update_sensor_limit/<token>', mehods = ['POST'])
+def update_sensor_limit(token):
+    if not __validate_token(token):
+        return jsonify({'error': f'Bad token {token}'}), 405
+    
+    if not __validate_sensor(token, None):
+        return jsonify({'error': 'No sensor attached'}), 405
+    
+    limit = request.args.get('limit')
+    __cache_set(f'{TOKEN_LIMIT_KEY}_{token}', limit)
+
+    return jsonify({'success': 'success'}), 200
 
 if __name__ == "__main__":
     app.run()
