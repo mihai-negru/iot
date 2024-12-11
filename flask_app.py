13dbcf9ce1230f2cb11dafea57f63d5d2f551ede
mihai-negru <determinant289@gmail.com>
2024-12-11 18:13:51 +0200

    Replaced to caching

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 #!/bin/usr/python
 
 from flask import Flask, request, render_template, jsonify
+from flask_caching import Cache
 import threading
 import json
 from datetime import datetime, timedelta
@@ -8,18 +9,26 @@ import os
 import uuid
 import shutil
 
+DATA_FILE = '/var/www/html/iot/data'
+TOKEN_KEY = 'TOKENS'
+TOKEN_SENSOR_KEY = 'TOKEN_SENSOR'
+TOKEN_DATA_KEY = 'TOKEN_DATA'
+
+config = {
+    'CACHE_TYPE': 'FileSystemCache',
+    'CACHE_DIR': DATA_FILE,
+    'CACHE_DEFAULT_TIMEOUT': 300
+}
+
 app = Flask(__name__)
+app.config.from_mapping(config)
+cache = Cache(app)
 
 app.DATA_FILE = '/var/www/html/iot/data'
 
-app.TOKENS_LOCK = threading.Lock()
-app.TOKENS = []
-app.TOKENS_DATA_LOCKS = {}
-app.TOKENS_SENSOR = {}
-
-if os.path.exists(app.DATA_FILE):
-    for filename in os.listdir(app.DATA_FILE):
-        file_path = f'{app.DATA_FILE}/{filename}'
+if os.path.exists(DATA_FILE):
+    for filename in os.listdir(DATA_FILE):
+        file_path = f'{DATA_FILE}/{filename}'
         try:
             if os.path.isdir(file_path):
                 shutil.rmtree(file_path)
@@ -27,6 +36,17 @@ if os.path.exists(app.DATA_FILE):
                 os.remove(file_path)
         except Exception:
             pass
+
+def __cache_get(key):
+    value = cache.get(key)
+    if value is None:
+        return None
+    
+    return json.loads(value)
+
+def __cache_set(key, value, timeout=0):
+    value = json.dumps(value)
+    cache.set(key, value, timeout = timeout)
 
 @app.route('/', methods = ['GET'])
 def homepage():
@@ -36,9 +56,13 @@ def homepage():
 def generate_token():
     new_token = str(uuid.uuid4())
 
-    with app.TOKENS_LOCK:
-        app.TOKENS.append(new_token)
-    app.TOKENS_DATA_LOCKS[new_token] = threading.Lock()
+    tokens = __cache_get(TOKEN_KEY)
+    if tokens is None:
+        tokens = [new_token]
+    else:
+        tokens.append(new_token)
+
+    __cache_set(TOKEN_KEY, tokens)
 
     return render_template(
         template_name_or_list = 'generated_token.html',
@@ -46,22 +70,18 @@ def generate_token():
     )
 
 def __validate_token(token):
-    with app.TOKENS_LOCK:
-        return token in app.TOKENS
+    tokens = __cache_get(TOKEN_KEY)
+    if tokens is None:
+        return False
+    
+    return token in tokens
 
 def __validate_sensor(token, sensor):
-    if token not in app.TOKENS_SENSOR:
-        return False
-    
-    if sensor is not None and app.TOKENS_SENSOR[token][0] != sensor:
+    sensor_ip = __cache_get(f'{TOKEN_SENSOR_KEY}_{token}')
+    if sensor_ip is None:
         return False
 
-    current_time = datetime.now()
-    if current_time >= app.TOKENS_SENSOR[token][1]:
-        del app.TOKENS_SENSOR[token]
-        return False
-    
-    return True
+    return sensor == sensor_ip
 
 @app.route('/view', methods = ['GET'])
 def view():
@@ -85,19 +105,14 @@ def view():
     )
 
 def __read__token_data(token):
-    try:
-        with app.TOKENS_DATA_LOCKS[token]:
-            with open(f'{app.DATA_FILE}/{token}', 'r') as fin:
-                data = json.load(fin)
-
-        return data
-    except Exception:
+    content = __cache_get(f'{TOKEN_DATA_KEY}_{token}')
+    if content is None:
         return []
     
+    return content
+    
 def __write_token_data(token, data):
-    with app.TOKENS_DATA_LOCKS[token]:
-        with open(f'{app.DATA_FILE}/{token}', 'w') as fout:
-                json.dump(data, fout)
+    __cache_set(f'{TOKEN_DATA_KEY}_{token}', data)
 
 @app.route('/view_data/<token>', methods = ['GET'])
 def view_data(token):
@@ -110,8 +125,8 @@ def view_data(token):
     data = __read__token_data(token)
     return jsonify(data), 200
 
-def __update_token_sensor_time(token):
-    app.TOKENS_SENSOR[token] = (app.TOKENS_SENSOR[token][0], datetime.now() + timedelta(minutes=10))
+def __update_token_sensor_time(token, sensor_ip):
+    __cache_set(f'{TOKEN_SENSOR_KEY}_{token}', sensor_ip, timeout=600)
 
 @app.route('/update_data/<token>', methods = ['POST'])
 def update_data(token):
@@ -121,7 +136,7 @@ def update_data(token):
     if not __validate_sensor(token, request.remote_addr):
         return jsonify({'error': 'Mismatch in sensors'}), 405
     
-    __update_token_sensor_time(token)
+    __update_token_sensor_time(token, request.remote_addr)
     
     body = request.json
     if 'values' not in body:
@@ -146,7 +161,7 @@ def alive(token):
     if not __validate_token(token):
         return jsonify({'error': f'Bad token {token}'}), 405
     
-    app.TOKENS_SENSOR[token] = (request.remote_addr, datetime.now() + timedelta(minutes=10))
+    __update_token_sensor_time(token, request.remote_addr)
 
     return jsonify({'success': f'Registered sensor for token {token} for 10 min'}), 200
 
